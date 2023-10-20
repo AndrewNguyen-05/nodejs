03b38d0c96a9ac7e622432387e27dae97c6e0192
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-10-21 00:03:04 +0700

    Set up .env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express"); //commonjs
 const path = require("path"); //commonjs
+require("dotenv").config(); //setting up to use env
+
+console.log(">>> Check env: ", process.env);
+
 const app = express(); //app express
-const port = 8080;
+const port = process.env.PORT || 8888; //port
+const hostname = process.env.HOST_NAME || "localhost"; //host
 
 //config template engine
 app.set("views", path.join(__dirname, "views"));
@@ -16,6 +21,6 @@ app.get("/abc", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
