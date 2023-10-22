d423ec9843c9ebd4fea33a8efabd72c3ea9ae191
Andrew Nguyen <21520144@gm.uit.edu.vn>
2023-10-22 20:10:06 +0700

    Add database MySQL

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express"); //commonjs
 require("dotenv").config(); //setting up to use env
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express(); //app express
 const port = process.env.PORT || 8888; //port
@@ -12,6 +13,21 @@ configViewEngine(app);
 
 //Khai báo route
 app.use("/", webRoutes);
+
+//create connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+// simple query
+connection.query("SELECT * FROM Users u", function (err, results, fields) {
+  console.log(">>> results:", results); // results contains rows returned by server
+  console.log(">>> fields:", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
